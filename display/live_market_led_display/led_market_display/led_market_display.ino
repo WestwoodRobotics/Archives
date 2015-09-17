58c519ad50531ee29e23269673e10e55d9356598
Qrules <Qrules@users.noreply.github.com>
2015-09-16 21:20:10 -0500

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   display/live_market_led_display/led_market_display/led_market_display.ino
    
    Got it working with 2 strips, decreased lag time to around 1/2 seconds

--- display/live_market_led_display/led_market_display/led_market_display.ino
+++ display/live_market_led_display/led_market_display/led_market_display.ino
@@ -1,13 +1,16 @@
+//Created by Qrules for Westwood Robotics under GNU GPL v3
+//Based on adafruit libraries for neoixel led modules
+
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 #include <LiquidCrystal.h>
-
+#define NUMPIXELS 120
 #define PIN 6
 LiquidCrystal lcd(12,11,5,4,3,2);
-#define poten 3
+#define POTENTIOMETER 3
 
 
 
@@ -26,7 +29,7 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -49,9 +52,9 @@ void setup() {
 }
 
 void loop() {
-    float val = analogRead(poten);
-    float percent = (int)(200 * (val + 1)/1020);
-    float pixels = (30 * (percent/100)) - 1;
+    float val = analogRead(POTENTIOMETER);
+    float percent = (int)(200 * (val)/1020);
+    float pixels = (30 * (percent/100));
     int pixelInt = (int)pixels;
     if (pixelInt < 0) {
       pixelInt = 0;
@@ -66,101 +69,25 @@ void loop() {
     lcd.print ("%");
 
 
-rainbow(3, pixelInt);
-//rainbow(3, pixels);
-//theaterChaseRainbow(10);
-   
-    //delay(175);
+rainbow(0, pixelInt);
 
 
-  /*
-
-  
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127, 0, 0), 50); // Red
-  theaterChase(strip.Color(0, 0, 127), 50); // Blue
-
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
-
-  */
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait, int setpix) {
-  for(uint16_t i=setpix; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
 }
 
 void rainbow(uint8_t wait, int setpix) {
   uint16_t i, j;
-
+  for(int k = 0; k < NUMPIXELS; k++) {
+     strip.setPixelColor(k, strip.Color(0,0,0)); //Black or off
+  }
+  
   for(j=0; j<256; j++) {
-    for(i=0; i<setpix; i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-  colorWipe(strip.Color(0, 0, 0), 0, setpix);
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      for(i=0; i<setpix; i++) {
+        strip.setPixelColor(i, Wheel((i+j) & 255));
+        strip.setPixelColor(NUMPIXELS - 1 - i, Wheel((i+j) & 255));
       }
-      strip.show();
-
-      delay(wait);
-
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
+  strip.show();
+  delay(wait);
+  } 
 }
 
 // Input a value 0 to 255 to get a color value.
