1ecd69ec518a1c6524b23de34d40a6ece9c19036
Overlord-Cob <chase@chaseblock.com>
2015-10-03 09:13:19 -0500

    Fixed the broken flow of the program by rearranging function orders.

--- robot/robotc/actions.c
+++ robot/robotc/actions.c
@@ -1,15 +1,15 @@
-// Runs the start-up stuffs
-void init(){
-	// Assume that the door is open so the it can be closed
-	doorOpen = true;
-	// Close the back door
-	toggle_door();
-}
-
 // Toggles the state of the back door
 void toggle_door(){
 	// If the door is open, close it, if it is closed, open it
 	motor[doorServo] = doorOpen ? doorClosePos : doorOpenPos;
 	// Change the boolean tracker to reflect the new state
 	doorOpen = !doorOpen;
+}
+
+// Runs the start-up stuffs
+void init(){
+	// Assume that the door is open so the it can be closed
+	doorOpen = true;
+	// Close the back door
+	toggle_door();
 }
--- robot/robotc/drive.c
+++ robot/robotc/drive.c
@@ -1,3 +1,15 @@
+// This method moves both wheels while taking input from only the right joystick
+void sync_drive(){
+	motor[leftMotor] = DEADBAND(vexRT[JOY_AXIS_RIGHT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
+	motor[rightMotor] = DEADBAND(vexRT[JOY_AXIS_RIGHT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
+}
+
+// This method moves each wheel independently, taking input from the respective joysticks
+void tank_drive(){
+	motor[leftMotor] = DEADBAND(vexRT[JOY_AXIS_LEFT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
+	motor[rightMotor] = DEADBAND(vexRT[JOY_AXIS_RIGHT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
+}
+
 // Takes input from the driver to move the robot
 // This task operates concurrently with the main method
 task drive(){
@@ -11,16 +23,4 @@ task drive(){
 			tank_drive();
 		}
 	}
-}
-
-// This method moves both wheels while taking input from only the right joystick
-void sync_drive(){
-	motor[leftMotor] = DEADBAND(vexRT[JOY_AXIS_RIGHT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
-	motor[rightMotor] = DEADBAND(vexRT[JOY_AXIS_RIGHT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
-}
-
-// This method moves each wheel independently, taking input from the respective joysticks
-void tank_drive(){
-	motor[leftMotor] = DEADBAND(vexRT[JOY_AXIS_LEFT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
-	motor[rightMotor] = DEADBAND(vexRT[JOY_AXIS_RIGHT]) * (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1);
 }
