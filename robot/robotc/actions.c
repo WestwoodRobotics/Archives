8e28eb0217b8c9526011e59beed4cb8ee227885c
Overlord-Cob <chase@chaseblock.com>
2015-10-19 17:38:21 -0500

    Fixed wierd stuffs

--- robot/robotc/actions.c
+++ robot/robotc/actions.c
@@ -2,7 +2,6 @@
 task dustpan(){
 	while(true){
 		// The lift is controlled by the up or down buttons on both sides of the controller
-<<<<<<< HEAD
 		int up = vexRT[DUST_UP];													// Input for up
 		int down = vexRT[DUST_UP];												// Input for down
 		motor[dustPan] = (up - down) * BTN_SPEED;					// Sets the dust pan motor's speed
@@ -11,14 +10,9 @@ task dustpan(){
 
 task fork_lift(){
 	while(true){
-		int up2 = vexRT[FORK_UP];													// Input for up
-		int down2 = vexRT[FORK_DOWN];											// Input for down
-		motor[forkLift] = (up2 - down2) * BTN_SPEED;			// Sets the fork lift's motor's speed
-=======
-		int up = vexRT[DUSTPAN_UP];												// Input for up
-		int down = vexRT[DUSTPAN_DOWN];										// Input for down
-		motor[dustpanMotor] = (up - down) * BTN_SPEED;					// Sets the left motor's speed
->>>>>>> 30761cac855f0f53cdd4b99819748e904a24f9f9
+		int up = vexRT[FORK_UP];													// Input for up
+		int down = vexRT[FORK_DOWN];											// Input for down
+		motor[forkLift] = (up - down) * BTN_SPEED;			// Sets the fork lift's motor's speed
 	}
 }
 
--- robot/robotc/config.h
+++ robot/robotc/config.h
@@ -35,7 +35,6 @@
 #define DUSTPAN_UP Btn7U			// Raises the dustpan
 #define DUSTPAN_DOWN Btn7D		// Lowers the dustpan
 
-<<<<<<< HEAD
 // Control the dustpan
 #define DUST_UP Btn8U					// Moves the dust pan up
 #define DUST_DOWN Btn8D				// Moves the dust pan down
@@ -43,35 +42,23 @@
 // Control the forklift
 #define FORK_UP Btn7U					// Moves the forklift up
 #define FORK_DOWN Btn7D				// Moves the forklift down
-=======
 // Control the forklift
 #define FORKLIFT_UP Btn8U			// Raises the forklift
 #define FORKLIFT_DOWN Btn8D		// Lowers the forklift
->>>>>>> 30761cac855f0f53cdd4b99819748e904a24f9f9
 
 /****Constants with numbers****/
 #define SLOW_MOD 0.5					// The relative slow speed of the robot
 #define BTN_SPEED 127					// A modifier to multiply the button inputs by for motors
 
-<<<<<<< HEAD
 // Defines the positions for the opened and closed positions for the back door
 #define doorClosePos 127
 #define doorOpenPos (-1 * doorClosePos)
 
-=======
->>>>>>> 30761cac855f0f53cdd4b99819748e904a24f9f9
 /****Variables & Such: They Change****/
 bool arcade = false;					// Tracks the drive mode for the robot
 
 // Checks for slow and reverse modes based on button input
-<<<<<<< HEAD
 #define SLOW (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1)
 #define REVERSE (vexRT[REVERSE_BTN] > 0 ? -1 : 1)
 
-#endif
-=======
-#define SLOW (vexRT[SLOW_BTN] > 0 ? SLOW_MOD : 1) // Gets the current slow modifier
-#define REVERSE (vexRT[REVERSE_BTN] > 0 ? -1 : 1) // Gets the current reverse modifier
-
 #endif // RCONFIG
->>>>>>> 30761cac855f0f53cdd4b99819748e904a24f9f9
