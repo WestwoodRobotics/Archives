f6477e3bf3eeaeec210e9374cc48c4d8ce950b23
Chase Block <git@chaseblock.com>
2015-11-10 19:55:04 -0600

    Added some of the power back to the pan.

--- robot/robotc/actions.c
+++ robot/robotc/actions.c
@@ -45,7 +45,7 @@ task dust_pan(){
 		// The lift is controlled by the up or down buttons on both sides of the controller
 		int up = vexRT[DUST_UP];													// Input for up
 		int down = vexRT[DUST_DOWN];												// Input for down
-		motor[dustPan] = (up - down) * BTN_SPEED / 2;					// Sets the dust pan motor's speed
+		motor[dustPan] = (up - down) * BTN_SPEED * 0.8;					// Sets the dust pan motor's speed
 	}
 }
 
