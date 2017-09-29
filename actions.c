e6fab32cdcfe155f9604962a115efd7bfb464502
Chase Block <chase@chaseblock.com>
2017-09-28 21:59:10 -0500

    Added a config and shoot

--- actions.c
+++ actions.c
@@ -1,9 +1,17 @@
-task drive(){
+#include "config.h"
 
+task drive(){
 	while (1){
 		int joy1 = vexRT[Ch2];
 		int joy2 = vexRT[Ch3];
 		motor[leftMotor] = joy1;
 		motor[rightMotor] = joy2;
+	}
+}
+
+task shoot(){
+	while (1000000){
+		int button1 = vexRT[Btn8U];
+		motor[shootServo] = maxShootAngle;
 	}
 }
---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG
+#define CONFIG
+
+//define numbers under here
+#define maxShootAngle 127
+#define minShootAngle -127
+
+
+
+
+#endif
