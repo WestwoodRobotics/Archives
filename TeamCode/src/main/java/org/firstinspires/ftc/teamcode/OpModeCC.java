2e79f696d6b5a5a14980ea728ddc8bf20101e365
Christian Gates <ctg143sharkes@gmail.com>
2020-11-18 19:18:03 -0600

    Changed the way motor values were being set to keep the values in the same proportional relationship between one another when they are outside the range of -1 to 1, instead of just clipping the outside values. Removed the Range.clip() calls from the input filter equations and added code to find the greatest value between the absolute values of the original power values (original refering to the values after the filter equations) and then set each motor's power to the original power divided by the greatest value between the abs values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeCC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeCC.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import javax.swing.JOptionPane;
+
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 public class OpModeCC extends OpMode
 {
@@ -14,6 +16,12 @@ public class OpModeCC extends OpMode
     private DcMotor frontRightDrive;
     private DcMotor backLeftDrive;
     private DcMotor backRightDrive;
+
+
+    private double frontLeftPower;
+    private double frontRightPower;
+    private double backLeftPower;
+    private double backRightPower;
 
     @Override
     public void init() {
@@ -24,11 +32,19 @@ public class OpModeCC extends OpMode
         backLeftDrive = hardwareMap.get(DcMotor.class,"backLeft");
         backRightDrive = hardwareMap.get(DcMotor.class,"backRight");
 
+
         //Set the motor direction
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+
+        //Set the motor powers to 0 by default
+        frontLeftPower = 0;
+        frontRightPower = 0;
+        backLeftPower = 0;
+        backRightPower = 0;
 
 
         telemetry.addData("Status", "Initialized");
@@ -41,11 +57,36 @@ public class OpModeCC extends OpMode
         //use gamepad1.left_stick_x and gamepad1.left_stick_y to get the current x and y positions
         //of the left stick on the first gamepad.
 
-        //Map joystick x and y positions to motor powers
-        frontLeftDrive.setPower(Range.clip(gamepad1.left_stick_x + (gamepad1.left_stick_y*-1) + gamepad1.right_stick_x,-1,1));
-        frontRightDrive.setPower(Range.clip((gamepad1.left_stick_y*-1) - gamepad1.left_stick_x - gamepad1.right_stick_x,-1,1));
-        backLeftDrive.setPower(Range.clip((gamepad1.left_stick_y*-1) - gamepad1.left_stick_x + gamepad1.right_stick_x,-1,1));
-        backRightDrive.setPower(Range.clip(gamepad1.left_stick_x + (gamepad1.left_stick_y*-1) - gamepad1.right_stick_x,-1,1));
+        //Get the values of the joystick input filtered through the corresponding equation for each wheel/motor
+        frontLeftPower = gamepad1.left_stick_x + (gamepad1.left_stick_y*-1) + gamepad1.right_stick_x;
+        frontRightPower = (gamepad1.left_stick_y*-1) - gamepad1.left_stick_x - gamepad1.right_stick_x;
+        backLeftPower = (gamepad1.left_stick_y*-1) - gamepad1.left_stick_x + gamepad1.right_stick_x;
+        backRightPower = gamepad1.left_stick_x + (gamepad1.left_stick_y*-1) - gamepad1.right_stick_x;
+
+        //Keep the motor powers between -1 and 1 (inclusive) without changing the proportion between the values
+        double greatestPower = Math.abs(FrontLeftPower);
+        if (Math.abs(frontRightPower) > greatestPower) {
+            greatestPower = Math.abs(frontRightPower);
+        }
+        if (Math.abs(backLeftPower) > greatestPower) {
+            greatestPower = Math.abs(backLeftPower);
+        }
+        if (Math.abs(backRightPower) > greatestPower) {
+            greatestPower = Math.abs(backRightPower);
+        }
+        if (greatestPower > 1) {
+            frontLeftPower /= greatestPower;
+            frontRightPower /= greatestPower;
+            backLeftPower /= greatestPower;
+            backRightPower /= greatestPower;
+        }
+
+        //Set the motor powers to their corresponding values
+        frontLeftDrive.setPower(frontLeftPower);
+        frontRightDrive.setPower(frontRightPower);
+        backLeftDrive.setPower(backLeftPower);
+        backRightDrive.setPower(backRightPower);
+
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.update();
