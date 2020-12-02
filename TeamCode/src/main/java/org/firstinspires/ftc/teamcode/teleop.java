2c0873ded79513ae836509bbc76d72baf645ebfe
Monika Gupta <monikagupta@Monikas-MacBook-Air.local>
2020-12-01 18:37:11 -0600

    Programmed the rest of the mechanisms for teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -17,8 +19,21 @@ public class teleop extends OpMode
     private DcMotor rightBackDrive = null;
     private DcMotor leftFrontDrive = null;
     private DcMotor rightFrontDrive = null;
+    private DcMotor conveyorMotor = null;
+    private DcMotor shooterMotor = null;
+    private DcMotor intakeMotor = null;
+    private Servo wobbleClaw = null;
+    private Servo wobbleClaw2 = null;
+    private Servo shooterAngle = null;
+
+
     private double scalar = 1;
     private boolean isdpadpressed = false;
+    private boolean isdpadpressedtwo = false;
+    private double shooterangleposition = 135;
+
+
+
 
     @Override
     public void init() {
@@ -28,10 +43,19 @@ public class teleop extends OpMode
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
         leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFront");
+//        conveyorMotor = hardwareMap.get(DcMotor.class, "conveyorMotor");
+//        shooterMotor = hardwareMap.get(DcMotor.class,"shooterMotor");
+//        intakeMotor = hardwareMap.get(DcMotor.class,"intakeMotor");
+//
+//        wobbleClaw = hardwareMap.get(Servo.class,"wobbleClaw");
+//        wobbleClaw2 = hardwareMap.get(Servo.class,"wobbleClaw2");
+//        shooterAngle = hardwareMap.get(Servo.class,"shooterAngle");
+
+
 
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.update();
@@ -69,6 +93,80 @@ public class teleop extends OpMode
         rightBackPower = -gamepad1.left_stick_y + gamepad1.left_stick_x -turnPower;
         leftFrontPower =-gamepad1.left_stick_y + gamepad1.left_stick_x + turnPower;
         leftBackPower = -gamepad1.left_stick_y - gamepad1.left_stick_x + turnPower;
+
+        if(gamepad2.a ) {
+        conveyorMotor.setPower(1);
+        }
+        else if (gamepad2.b) {
+            conveyorMotor.setPower(-1);
+        }
+        else {
+                conveyorMotor.setPower(0);
+        }
+
+        if(gamepad2.x) {
+            wobbleClaw.setPosition(20);
+        }
+        else if  (gamepad2.y) {
+            wobbleClaw.setPosition(90);
+        }
+        if(gamepad2.right_trigger >0 ) {
+            wobbleClaw2.setPosition(90);
+        }
+        if(gamepad2.left_trigger>0) {
+            wobbleClaw2.setPosition(0);
+        }
+
+       if(gamepad1.a) {
+        intakeMotor.setPower(1);
+       }
+       else if(gamepad1.b) {
+           intakeMotor.setPower(-1);
+       }
+
+
+       if(gamepad2.left_bumper) {
+           shooterMotor.setPower((1));
+       }
+       else if(gamepad2.right_bumper)  {
+           shooterMotor.setPower(0);
+       }
+
+
+       shooterAngle.setPosition(shooterangleposition);
+
+       if (!isdpadpressedtwo) {
+            if (gamepad2.dpad_up) {
+                if (shooterangleposition < 180) {
+                    shooterangleposition -= 5;
+                    isdpadpressedtwo = true;
+                }
+            }
+            else if (gamepad2.dpad_down ) {
+                if (shooterangleposition > 0) {
+                    shooterangleposition += 5;
+                    isdpadpressedtwo = true;
+                }
+            }
+            else if (gamepad2.dpad_left) {
+                shooterangleposition = 30;
+                isdpadpressedtwo = true;
+            }
+            else if (gamepad2.dpad_right) {
+                shooterangleposition = 135;
+                isdpadpressedtwo = true;
+            }
+
+        }
+        else if(!gamepad2.dpad_up && !gamepad2.dpad_down && !gamepad2.dpad_right && !gamepad2.dpad_left)  {
+            isdpadpressedtwo = false;
+        }
+
+        shooterAngle.setPosition(shooterangleposition);
+      
+
+
+
 
 
         double greatestPower = Math.abs(rightFrontPower);
