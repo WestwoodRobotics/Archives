bd7f701aed00a1847eb5a6197a0bd9f82635855c
Christian11705 <ctg143sharks@gmail.com>
2020-12-08 16:52:55 -0600

    Created variable to store if the intake is on are off, added some comments, and initialized the intake motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeCC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeCC.java
@@ -10,16 +10,23 @@ import com.qualcomm.robotcore.util.Range;
 public class OpModeCC extends OpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
+    //Initialize drive train motors
     public DcMotor frontLeftDrive;
     public DcMotor frontRightDrive;
     public DcMotor backLeftDrive;
     public DcMotor backRightDrive;
 
-
+    //Create motor power variables for drive train motors
     private double frontLeftPower;
     private double frontRightPower;
     private double backLeftPower;
     private double backRightPower;
+
+    //Initialize intake motor and create the power variable
+    private DcMotor intakeMotor;
+    private double intakeMotorPower;
+
+    private boolean intakeOn;
 
     @Override
     public void init() {
@@ -30,12 +37,16 @@ public class OpModeCC extends OpMode
         backLeftDrive = hardwareMap.get(DcMotor.class,"leftBack");
         backRightDrive = hardwareMap.get(DcMotor.class,"rightBack");
 
+        intakeMotor = hardwareMap.get(DcMotor.class,"intake");
 
-        //Set the motor direction
+
+        //Set the motor directions
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
 
 
         //Set the motor powers to 0 by default
@@ -44,11 +55,14 @@ public class OpModeCC extends OpMode
         backLeftPower = 0;
         backRightPower = 0;
 
+        //Set the zero power behavior of the motors to stop quickly
+        stop();
+
+        //Create a boolean to store rather or not the intake is turned on or off, and set it to false by default
+        intakeOn = false;
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-        stop();
     }
 
     @Override
@@ -58,16 +72,16 @@ public class OpModeCC extends OpMode
         of the left stick on the first gamepad.*/
 
         //Get the values of the joystick input filtered through the corresponding equation for each wheel/motor
-
         frontLeftPower = gamepad1.left_stick_x - gamepad1.left_stick_y + gamepad1.right_stick_x;
         frontRightPower = -gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x;
         backLeftPower = -gamepad1.left_stick_x - gamepad1.left_stick_y + gamepad1.right_stick_x;
         backRightPower = gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x;
 
-//        frontLeftPower =  gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x;
-//        frontRightPower=  gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x;
-//        backLeftPower  = -gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
-//        backRightPower = -gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x;
+        /* Incorrect equations:
+        frontLeftPower =  gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x;
+        frontRightPower=  gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x;
+        backLeftPower  = -gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
+        backRightPower = -gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x; */
 
         //Keep the motor powers between -1 and 1 (inclusive) without changing the proportion between the values
         double greatestPower = Math.abs(frontLeftPower);
@@ -87,6 +101,7 @@ public class OpModeCC extends OpMode
             backRightPower /= greatestPower;
         }
 
+        //Slo-Mo when holding LB
         if (gamepad1.left_bumper){
             frontLeftPower *= 0.5;
             frontRightPower *= 0.5;
