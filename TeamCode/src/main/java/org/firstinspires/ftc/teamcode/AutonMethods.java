203a861340c582cff621b9deb357fc2c09b84d76
jmalwade <javd722@gmail.com>
2020-12-30 20:25:38 -0600

    turn auton methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMethods.java
@@ -7,6 +7,7 @@ public class AutonMethods {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeft, frontRight, backLeft, backRight;
     private final int INCHES_TO_TICKS = 5;
+    private final int DEGREES_TO_TICKS = 360;
 
     public AutonMethods(DcMotor frontLeft, DcMotor frontRight, DcMotor backLeft, DcMotor backRight) {
         this.frontLeft = frontLeft;
@@ -16,26 +17,97 @@ public class AutonMethods {
     }
 
     public void goForward(double inches) {
-        boolean isAllBusy = frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy();
         int ticks = (int) (inches * INCHES_TO_TICKS);
 
         //reset encoders
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        resetAllEncoders();
 
         //set target
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(-ticks);
+        backLeft.setTargetPosition(-ticks);
+        backRight.setTargetPosition(ticks);
+
+        //RUN_TO_POSITION mode
+        runToPosition();
+        //set power
+        frontLeft.setPower(1);
+        frontRight.setPower(-1);
+        backLeft.setPower(-1);
+        backRight.setPower(1);
+
+        while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+            //do nothing until target reached but maybe use PID here?
+        }
+
+        brakeAllMotors();
+        setToUsingEncoders();
+    }
+
+    public void goBackward(double inches) {
+        int ticks = (int) (inches * INCHES_TO_TICKS);
+
+        //reset encoders
+        resetAllEncoders();
+
+        //set target
+        frontLeft.setTargetPosition(-ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(-ticks);
+
+        //RUN_TO_POSITION mode
+        runToPosition();
+
+        //set power
+        frontLeft.setPower(-1);
+        frontRight.setPower(1);
+        backLeft.setPower(1);
+        backRight.setPower(-1);
+
+        while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+            //do nothing until target reached but maybe use PID here?
+        }
+
+        brakeAllMotors();
+        setToUsingEncoders();
+    }
+
+    public void goLeft(double inches) {
+        int ticks = (int) (inches * INCHES_TO_TICKS);
+        resetAllEncoders();
+
+        frontLeft.setTargetPosition(-ticks);
+        frontRight.setTargetPosition(-ticks);
+        backLeft.setTargetPosition(-ticks);
+        backRight.setTargetPosition(-ticks);
+
+        runToPosition();
+
+        //set power
+        frontLeft.setPower(-1);
+        frontRight.setPower(-1);
+        backLeft.setPower(-1);
+        backRight.setPower(-1);
+
+        while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+            //do nothing until target reached but maybe use PID here?
+        }
+
+        brakeAllMotors();
+        setToUsingEncoders();
+    }
+
+    public void goRight(double inches) {
+        int ticks = (int) (inches * INCHES_TO_TICKS);
+        resetAllEncoders();
+
         frontLeft.setTargetPosition(ticks);
         frontRight.setTargetPosition(ticks);
         backLeft.setTargetPosition(ticks);
         backRight.setTargetPosition(ticks);
 
-        //RUN_TO_POSITION mode
-        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        runToPosition();
 
         //set power
         frontLeft.setPower(1);
@@ -43,7 +115,57 @@ public class AutonMethods {
         backLeft.setPower(1);
         backRight.setPower(1);
 
-        while (isAllBusy) {
+        while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+            //do nothing until target reached but maybe use PID here?
+        }
+
+        brakeAllMotors();
+        setToUsingEncoders();
+    }
+
+    public void turnRightCenter(double degrees){
+        int ticks = (int) (degrees * DEGREES_TO_TICKS);
+        resetAllEncoders();
+
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(-ticks);
+        backRight.setTargetPosition(-ticks);
+
+        runToPosition();
+
+        //set power
+        frontLeft.setPower(1);
+        frontRight.setPower(1);
+        backLeft.setPower(-1);
+        backRight.setPower(-1);
+
+        while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
+            //do nothing until target reached but maybe use PID here?
+        }
+
+        brakeAllMotors();
+        setToUsingEncoders();
+    }
+
+    public void turnLeftCenter(double degrees) {
+        int ticks = (int) (degrees * DEGREES_TO_TICKS);
+        resetAllEncoders();
+
+        frontLeft.setTargetPosition(-ticks);
+        frontRight.setTargetPosition(-ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+
+        runToPosition();
+
+        //set power
+        frontLeft.setPower(-1);
+        frontRight.setPower(-1);
+        backLeft.setPower(1);
+        backRight.setPower(1);
+
+        while (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy()) {
             //do nothing until target reached but maybe use PID here?
         }
 
@@ -78,4 +200,23 @@ public class AutonMethods {
         backLeft.setPower(0);
         backRight.setPower(0);
     }
+
+    public void runToPosition() {
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void pause(double seconds) {
+        runtime.reset();
+        while(runtime.seconds() < seconds) {
+
+        }
+    }
+
+
+
+
+
 }
