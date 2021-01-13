98545d73181aa4a0f024ab6017e95fcf0f081b14
Monika Gupta <monikagupta@Monikas-MacBook-Air.local>
2021-01-12 18:30:28 -0600

    auton for blue

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonfunctionsB.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServoImpl;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+public class autonfunctionsB {
+
+    public DcMotor leftBackWheel = null;
+    public DcMotor leftFrontWheel = null;
+    public DcMotor rightFrontWheel = null;
+    public DcMotor rightBackWheel = null;
+    public DcMotor ringShooter = null;
+    public DcMotor ringIntake = null;
+    public DcMotor circleRamp = null;
+    public CRServoImpl wobbleClaw1 = null;
+    public CRServoImpl wobbleClaw2 = null;
+    public CRServoImpl angleChanger = null;
+    public ElapsedTime runtime1 = new ElapsedTime();
+
+}
+
+
+
+
+
+
