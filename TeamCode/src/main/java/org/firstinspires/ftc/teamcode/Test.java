3c76f63bd1ef8ffa68b386d597bdb6806a55e21b
Christian11705 <ctg143sharks@gmail.com>
2021-01-24 21:00:24 -0600

    halfved front motor powers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -60,8 +60,8 @@ public class Test {
     public void moveLeft () {
         double startTime = runtime.seconds();
         while (runtime.seconds() - startTime < 3) {
-            frontLeftDrive.setPower(-0.5);
-            frontRightDrive.setPower(0.5);
+            frontLeftDrive.setPower(-0.5/2);
+            frontRightDrive.setPower(0.5/2);
             backLeftDrive.setPower(0.5);
             backRightDrive.setPower(-0.5);
         }
@@ -71,8 +71,8 @@ public class Test {
     public void moveRight () {
         double startTime = runtime.seconds();
         while (runtime.seconds() - startTime < 3) {
-            frontLeftDrive.setPower(0.5);
-            frontRightDrive.setPower(-0.5);
+            frontLeftDrive.setPower(0.5/2);
+            frontRightDrive.setPower(-0.5/2);
             backLeftDrive.setPower(-0.5);
             backRightDrive.setPower(0.5);
         }
